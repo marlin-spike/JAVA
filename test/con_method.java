57d642ef4798fca4713a55526edbe9f3fc9670fc
Shreyash Gondane <50496886+marlin-spike@users.noreply.github.com>
2022-09-30 15:18:06 +0530

    Add files via upload

---
+++ test/con_method.java
@@ -0,0 +1,46 @@
+import java.util.*;
+
+class con_method
+{
+public static void main (String args[])
+	{
+		Power p1 = new Power();
+		Scanner sn = new Scanner(System.in);
+		
+		int f_no,s_no;
+		System.out.print("Enter a number : ");
+		f_no = sn.nextInt();
+
+		System.out.print(f_no+" To the power : ");
+		s_no = sn.nextInt();
+
+		p1.GetData(f_no ,s_no);
+		p1.powerOf();
+		p1.printdata();
+		
+	}
+}
+
+class Power
+{
+	int po = 1,A ,B ;
+
+	void GetData(int a ,int b)
+	{
+		A = a;
+		B = b;
+	}
+
+	void powerOf()
+	{
+		for(int i = 0 ; i<B ;i++ )
+		{
+			po = po *A;
+		}
+	}
+	void printdata()
+	{
+		System.out.print(A + " To the Power "+B+" is : "+po);
+	}
+
+}
