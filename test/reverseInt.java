63226d96b280c5dc41ea0ab7a46ca56f335c9645
Shreyash Gondane <50496886+marlin-spike@users.noreply.github.com>
2022-10-01 02:52:49 +0530

    Add files via upload

---
+++ test/reverseInt.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class reverseInt 
+{
+public static void main  (String args[])
+{
+	int i , rev= 0;
+	Scanner scan =new Scanner(System.in);
+	
+	System.out.print("Enter a String (int) : ");
+	int a = scan.nextInt();
+
+	while(a != 0)
+	{
+
+	i = a % 10;
+	rev = rev*10 + i;
+	a = a / 10;
+	
+	}
+System.out.println("the reverse is " + rev);
+}
+}
